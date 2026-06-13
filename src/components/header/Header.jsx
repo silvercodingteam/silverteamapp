362bb8df4add66e0b3aa185db27172499268985b
Silver Team <silvercodingteam@gmail.com>
2026-06-13 21:28:29 +0330

    responsive done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,14 +12,14 @@ export default function Header() {
 
     return (
         <header className="bg-black grid grid-cols-2 w-full min-h-30 rounded-b-full shadow-xl/30 text-white">
-            <div className="flex justify-start items-center px-15 gap-5">
+            <div className="flex justify-start items-center pr-15 gap-5">
                 <Image src={logo} alt="logo" width={640} height={640} className="w-20 h-20"/>
-                <h1 className="text-6xl">تیم سیلور</h1>
+                <h1 className="w-full text-3xl sm:text-5xl lg:text-6xl">تیم سیلور</h1>
             </div>
-            <div className="flex justify-end items-center px-15">
+            <div className="flex justify-end items-center pl-15">
                 <ul className="flex justify-end items-center flex-row gap-5 w-full h-full">
-                    <li className="text-2xl">نمونه کارها</li>
-                    <li className="text-2xl cursor-pointer" onClick={scrollToFooter}>ارتباط با ما</li>
+                    <li className="text-xl sm:text-2xl">نمونه کارها</li>
+                    <li className="text-xl sm:text-2xl cursor-pointer" onClick={scrollToFooter}>ارتباط با ما</li>
                 </ul>
             </div>
         </header>
